0eef26f497a2b97d4b403ab3da6380a31b52990a
Matthew Parker <matt_parker@outlook.com>
2026-04-13 11:16:29 -0400

    v1.10.5: sync version across all 18 targets

--- installer/src/OpenCut.Installer/Models/AppConstants.cs
+++ installer/src/OpenCut.Installer/Models/AppConstants.cs
@@ -3,7 +3,7 @@ namespace OpenCut.Installer.Models;
 public static class AppConstants
 {
     public const string AppName = "OpenCut";
-    public const string AppVersion = "1.10.4";
+    public const string AppVersion = "1.10.5";
     public const string AppDisplayName = $"{AppName} v{AppVersion}";
     public const string AppPublisher = "SysAdminDoc";
     public const string AppUrl = "https://github.com/SysAdminDoc/OpenCut";
